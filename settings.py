8475af59d4960062bdfee2a0ff59b55558592b32
gravic07 <grant@gravicdesign.com>
2015-09-12 12:58:42 -0700

    removing applicaiton IDs and client IDs

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = '551660766421-dseo06troprt3pmef1csumeruuecle8u.apps.googleusercontent.com'
+WEB_CLIENT_ID = 'replace with web client ID'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
